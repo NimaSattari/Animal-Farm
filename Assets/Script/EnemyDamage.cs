35a98e7c2c20df38b63cae48a5dc85b963185244
NimaSattari <54527075+NimaSattari@users.noreply.github.com>
2019-09-07 18:32:05 +0430

    Buffer

---
+++ Assets/Script/EnemyDamage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    [SerializeField] int hitPoints = 10;
+    [SerializeField] ParticleSystem hitParticlePrefab;
+    [SerializeField] ParticleSystem deathParticlePrefab;
+
+    void Start()
+    {
+        
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        if (hitPoints <= 0)
+        {
+            KillEnemy();
+        }
+    }
+
+    void ProcessHit()
+    {
+        hitPoints = hitPoints - 1;
+        hitParticlePrefab.Play();
+    }
+
+    private void KillEnemy()
+    {
+        var vfx = Instantiate(deathParticlePrefab, transform.position, Quaternion.identity);
+        vfx.Play();
+        Destroy(gameObject);
+    }
+}
